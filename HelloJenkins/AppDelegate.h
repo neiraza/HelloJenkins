10933ae90368d4821608e7b4b36fa42b2449bba7
toguri <tooru.oguri@gmail.com>
2011-12-23 22:11:31 +0900

    Initial Commit

---
+++ HelloJenkins/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloJenkins
+//
+//  Created by 徹 小栗 on 11/12/23.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloJenkins/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloJenkins
+//
+//  Created by 徹 小栗 on 11/12/23.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
